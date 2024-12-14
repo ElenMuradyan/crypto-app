767df10ff863699eb81899fa7833a6c045519f07
ElenMuradyan <muradyanelen0701@gmail.com>
2024-12-14 13:59:45 +0400

    fix(Header): fix location usage

--- src/components/global/Header/index.tsx
+++ src/components/global/Header/index.tsx
@@ -1,8 +1,6 @@
 import { Flex, Select, Typography } from 'antd';
 import { currencySelectOptions } from '../../../util/constants/currencySelectOptions';
 import { useQueryParam } from '../../../hooks/useQueryParam';
-import { useEffect } from 'react';
-import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { ROUTE_PATHS } from '../../../util/constants/routes';
 
@@ -12,17 +10,12 @@ const Header=()=>{
    const location = useLocation();
    const { setQueryParam, getQueryParam } = useQueryParam();
    const currency = getQueryParam('currency') || currencySelectOptions[0].value;
-   const [ locationHome, setLocationHome ] = useState<boolean>(true);
-
-   useEffect(() => {
-      setLocationHome(window.location.pathname === '/');
-   }, [window.location.pathname]);  
 
    return(
     <div className="main_header">
        <Flex justify="space-between" align="center">
        <div>
-       {!locationHome && <Link to={`${ROUTE_PATHS.HOME}${location.search}`}><Typography.Title level={5} style={{color: 'white', margin: 0}}>Home</Typography.Title></Link>}
+       {!(location.pathname === '/') && <Link to={`${ROUTE_PATHS.HOME}${location.search}`}><Typography.Title level={5} style={{color: 'white', margin: 0}}>Home</Typography.Title></Link>}
        </div>
        <Select
        className='select'
